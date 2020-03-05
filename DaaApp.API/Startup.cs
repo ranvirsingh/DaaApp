30cf52efb345decc183b54cdf5c70b0625ffb3c6
Ranvir Singh <ranvir.singh@nagarro.com>
2020-03-05 18:07:33 +0000

    Add exception handling to the application, server and client side
    End of Module 5

--- DaaApp.API/Startup.cs
+++ DaaApp.API/Startup.cs
@@ -7,6 +7,10 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using System.Net;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using DaaApp.API.Helpers;
 
 namespace DaaApp.API
 {
@@ -49,6 +53,25 @@ namespace DaaApp.API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler(
+                    builder =>
+                    {
+                        builder.Run(async context =>
+                        {
+                            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+                            var error = context.Features.Get<IExceptionHandlerFeature>();
+                            if (error != null)
+                            {
+                                context.Response.AddApplicationError(error.Error.Message);
+                                await context.Response.WriteAsync(error.Error.Message);
+                            }
+                        });
+                    }
+                );
             }
 
             // Used to redirect http to https
